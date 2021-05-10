0709ca31a5637f0e1dc6de9578bae9eb5f32999c
Rayell McMahan <rmcmahan1@hotmail.com>
2021-05-10 16:45:13 -0700

    adding while and for loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,8 +11,15 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
+    //repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
+        for(var i = 0; i < enemyNames.length; i++) {
+            //call fight function with enemy-robot
+            fight(enemyNames[i]);
+        }
+    }
 // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  //window.alert("Welcome to Robot Gladiators!");
 
     //ask player if they'd like to fight or run
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -68,5 +75,7 @@ var fight = function(enemyName) {
 //run fight function to start game
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames [i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
